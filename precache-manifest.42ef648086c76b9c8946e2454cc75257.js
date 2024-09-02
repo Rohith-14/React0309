929f6725bb2ab5d4128aa29b74e248f12c4e6ff0
Rohith Neralla <nvrrohith45@gmail.com>
2024-09-02 02:29:33 -0400

    Updates

---
+++ precache-manifest.42ef648086c76b9c8946e2454cc75257.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12c146ddc23860ee6f5ddc0a14519b9",
+    "url": "/Wise0309/index.html"
+  },
+  {
+    "revision": "72cf58496739170f97cb",
+    "url": "/Wise0309/static/css/main.50b4608b.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/Wise0309/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Wise0309/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "72cf58496739170f97cb",
+    "url": "/Wise0309/static/js/main.b5983612.chunk.js"
+  },
+  {
+    "revision": "e9083388e83d2a026cbb",
+    "url": "/Wise0309/static/js/runtime~main.95266d9e.js"
+  },
+  {
+    "revision": "d1a63beb829915bfa50badad6f03b02a",
+    "url": "/Wise0309/static/media/Nora.d1a63beb.mp4"
+  },
+  {
+    "revision": "0612f8aeffc516f3be4f6f7e382d85ad",
+    "url": "/Wise0309/static/media/butterfly.0612f8ae.jpeg"
+  }
+]);
